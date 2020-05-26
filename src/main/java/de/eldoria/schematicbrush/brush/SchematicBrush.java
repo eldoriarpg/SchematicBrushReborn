f4893422cfcde2e2ceb4a8e53206cf009a9eb691
RainbowDashLabs <mail@code-fox.de>
2020-05-26 03:20:30 +0200

    several bugfixes to get schematic brush working

--- src/main/java/de/eldoria/schematicbrush/brush/SchematicBrush.java
+++ src/main/java/de/eldoria/schematicbrush/brush/SchematicBrush.java
@@ -1,7 +1,10 @@
 package de.eldoria.schematicbrush.brush;
 
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.MaxChangedBlocksException;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.command.tool.brush.Brush;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.function.mask.BlockTypeMask;
@@ -11,6 +14,7 @@ import com.sk89q.worldedit.function.pattern.Pattern;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.math.transform.AffineTransform;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.session.PasteBuilder;
 import com.sk89q.worldedit.world.block.BlockTypes;
 import de.eldoria.schematicbrush.MessageSender;
 import de.eldoria.schematicbrush.schematics.Schematic;
@@ -50,37 +54,45 @@ public class SchematicBrush implements Brush {
             return;
         }
 
-        // Find center of schematic
-        BlockVector3 dimensions = clipboard.getDimensions();
-        int centerZ = dimensions.getBlockZ() / 2;
-        int centerX = dimensions.getBlockX() / 2;
-        int centerY = settings.getPlacement().find(clipboard);
-        clipboard.setOrigin(BlockVector3.at(centerX, centerY, centerZ));
-
+        // Apply flip
         Flip direction = randomBrushConfig.getFlip().getFlipDirection();
         AffineTransform transform = new AffineTransform();
         if (direction != Flip.NONE) {
             transform = transform.scale(direction.asVector().abs().multiply(-2).add(1, 1, 1));
         }
 
+        // Apply rotation
         Rotation rotation = randomBrushConfig.getRotation();
         transform = transform.rotateY(rotation.getDeg());
 
-        if (!settings.isReplaceAll()) {
+
+        // Apply replace mask
+        if (settings.isReplaceAirOnly()) {
             editSession.setMask(
                     new BlockTypeMask(editSession, BlockTypes.AIR, BlockTypes.VOID_AIR, BlockTypes.CAVE_AIR));
         }
 
-        clipboard.setOrigin(clipboard.getOrigin().add(0, settings.getYOffset(), 0));
+        // Create a new clipboard holder and apply the transforms
+        LocalSession localSession = WorldEdit.getInstance().getSessionManager().get(BukkitAdapter.adapt(brushOwner));
         ClipboardHolder clipboardHolder = new ClipboardHolder(clipboard);
-        clipboardHolder.setTransform(transform);
+        localSession.setClipboard(clipboardHolder);
+        clipboardHolder.setTransform(clipboardHolder.getTransform().combine(transform));
 
-        Operation operation = clipboardHolder
-                .createPaste(editSession)
-                .to(position)
+        // Find center of schematic and set with offset
+        BlockVector3 dimensions = clipboard.getDimensions();
+        int centerZ = clipboard.getMinimumPoint().getBlockZ() + dimensions.getBlockZ() / 2;
+        int centerX = clipboard.getMinimumPoint().getBlockX() + dimensions.getBlockX() / 2;
+        int centerY = clipboard.getMinimumPoint().getBlockY() + settings.getPlacement().find(clipboard);
+        clipboard.setOrigin(BlockVector3.at(centerX, centerY, centerZ));
+
+        // Create paste operation
+        PasteBuilder paste = clipboardHolder.createPaste(editSession);
+        Operation operation = paste
+                .to(position.add(0, settings.getYOffset(), 0))
                 .ignoreAirBlocks(!settings.isIncludeAir())
                 .build();
 
         Operations.completeBlindly(operation);
+        brushOwner.sendMessage("brush executed");
     }
 }
