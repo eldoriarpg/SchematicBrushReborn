6e48ed3ac2608ff8f560204051b30e3f424accc8
RainbowDashLabs <mail@code-fox.de>
2020-05-28 15:48:03 +0200

    mark message sender as utility class

--- src/main/java/de/eldoria/schematicbrush/MessageSender.java
+++ src/main/java/de/eldoria/schematicbrush/MessageSender.java
@@ -1,12 +1,14 @@
 package de.eldoria.schematicbrush;
 
+import lombok.experimental.UtilityClass;
 import org.bukkit.entity.Player;
 
+@UtilityClass
 public class MessageSender {
-    public static void sendMessage(Player p, String message) {
+    public void sendMessage(Player p, String message) {
         p.sendMessage("§6[SB] §4" + message);
     }
-    public static void sendError(Player p, String message) {
+    public void sendError(Player p, String message) {
         p.sendMessage("§6[SB] §c" + message);
     }
 }
