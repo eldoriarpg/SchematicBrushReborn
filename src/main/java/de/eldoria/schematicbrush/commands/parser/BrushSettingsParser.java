79296fed3215b68c659173c69c2973cfc1c138ae
RainbowDashLabs <mail@code-fox.de>
2020-06-02 21:12:27 +0200

    created tab util for schematic set conversion

--- src/main/java/de/eldoria/schematicbrush/commands/parser/BrushSettingsParser.java
+++ src/main/java/de/eldoria/schematicbrush/commands/parser/BrushSettingsParser.java
@@ -12,7 +12,6 @@ import lombok.experimental.UtilityClass;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,19 +20,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static de.eldoria.schematicbrush.commands.parser.ParsingUtil.parseToLegacySyntax;
+
 @UtilityClass
 public class BrushSettingsParser {
-    private final Pattern Y_OFFSET = Pattern.compile("-(?:(?:yoff)|(?:yoffset)|(?:y)):(-?[0-9]{1,3})$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern Y_OFFSET = Pattern.compile("-(?:(?:yoff)|(?:yoffset)|(?:y)):(-?[0-9]{1,3})$", Pattern.CASE_INSENSITIVE);
     private final Pattern PLACEMENT = Pattern.compile("-(?:(?:place)|(?:placement)|(?:p)):([a-zA-Z]+?)$", Pattern.CASE_INSENSITIVE);
 
-    // Legacy pattern parser
-    private final Pattern FLIP = Pattern.compile("-flip:|-f:", Pattern.CASE_INSENSITIVE);
-    private final Pattern ROTATION = Pattern.compile("-rotate:|-r:", Pattern.CASE_INSENSITIVE);
-    private final Pattern WEIGHT = Pattern.compile("-weight:|-w:", Pattern.CASE_INSENSITIVE);
-    private final Pattern RANDOM = Pattern.compile(":random|:r", Pattern.CASE_INSENSITIVE);
-    private final Pattern DIRECTORY = Pattern.compile("dir:|d:", Pattern.CASE_INSENSITIVE);
-    private final Pattern PRESET = Pattern.compile("preset:|p:", Pattern.CASE_INSENSITIVE);
-    private final Pattern REGEX = Pattern.compile("regex:|r:", Pattern.CASE_INSENSITIVE);
 
 
     public Optional<BrushSettings> parseBrush(Player player, Plugin plugin, SchematicCache schematicCache,
@@ -258,60 +251,6 @@ public class BrushSettingsParser {
             return Optional.of(plugin.getConfig().getStringList(path));
         }
         return Optional.empty();
-    }
-
-    private String[] parseToLegacySyntax(String[] args) {
-        List<String> parsedInput = new ArrayList<>();
-        boolean open = false;
-
-        StringBuilder argumentBuilder = new StringBuilder();
-        for (String arg : args) {
-            // If a string starts and ends with a double quote we assume, that a new schematic is defined inside
-            if (arg.startsWith("\"") && arg.endsWith("\"")) {
-                parsedInput.add(parseToLegacySelector(arg.substring(1, arg.length() - 1)));
-                continue;
-            }
-            // If a string starts with a double quote we assume, that a new schematic set will be defined and open the section
-            if (arg.startsWith("\"")) {
-                open = true;
-                argumentBuilder.append(parseToLegacySelector(arg.substring(1)));
-                continue;
-            }
-
-            // If a string starts with a double quote we assume, that a new schematic set was defined and close the section
-            if (arg.endsWith("\"")) {
-                open = false;
-                argumentBuilder.append(parseToLegacyModifier(arg.substring(0, arg.length() - 1)));
-                parsedInput.add(argumentBuilder.toString());
-                argumentBuilder.setLength(0);
-                continue;
-            }
-
-            // if the current section is not open we just add the input. Could be a flagt or legacy syntax
-            if (!open) {
-                parsedInput.add(arg);
-                continue;
-            }
-
-            // if the current section is open, we keep appending all arguments.
-            argumentBuilder.append(parseToLegacyModifier(arg));
-        }
-        return parsedInput.toArray(new String[0]);
-    }
-
-    private String parseToLegacyModifier(String modifier) {
-        String result = modifier;
-        result = FLIP.matcher(result).replaceAll("!");
-        result = ROTATION.matcher(result).replaceAll("@");
-        result = WEIGHT.matcher(result).replaceAll(":");
-        return RANDOM.matcher(result).replaceAll("*");
-    }
-
-    private String parseToLegacySelector(String selector) {
-        String result = selector;
-        result = DIRECTORY.matcher(selector).replaceAll("$");
-        result = PRESET.matcher(selector).replaceAll("&");
-        return REGEX.matcher(selector).replaceAll("^");
     }
 
 }
---
+++ src/main/test/de/eldoria/schematicbrush/commands/parser/ParsingUtilTest.java
@@ -0,0 +1,10 @@
+package de.eldoria.schematicbrush.commands.parser;
+
+import org.junit.Test;
+
+public class ParsingUtilTest {
+    @Test
+    public void test() {
+
+    }
+}
