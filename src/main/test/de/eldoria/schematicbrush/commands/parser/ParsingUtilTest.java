2d9c8749c4a2178e04089c24d78662f562cfe431
RainbowDashLabs <mail@code-fox.de>
2020-06-02 21:12:59 +0200

    add unit test for schematic set conversion

--- src/main/test/de/eldoria/schematicbrush/commands/parser/ParsingUtilTest.java
+++ src/main/test/de/eldoria/schematicbrush/commands/parser/ParsingUtilTest.java
@@ -1,10 +1,30 @@
 package de.eldoria.schematicbrush.commands.parser;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ParsingUtilTest {
     @Test
-    public void test() {
+    public void legacySelectorParserTest() {
+        Assert.assertEquals("$test", ParsingUtil.parseToLegacySelector("dir:test"));
+        Assert.assertEquals("$test*", ParsingUtil.parseToLegacySelector("dir:test*"));
+        Assert.assertEquals("^test.+?", ParsingUtil.parseToLegacySelector("regex:test.+?"));
+        Assert.assertEquals("&test", ParsingUtil.parseToLegacySelector("preset:test"));
+        Assert.assertEquals("test", ParsingUtil.parseToLegacySelector("test"));
+    }
 
+    @Test
+    public void legacyModifierParserTest() {
+        Assert.assertEquals("@180", ParsingUtil.parseToLegacyModifier("-rotate:180"));
+        Assert.assertEquals("@*", ParsingUtil.parseToLegacyModifier("-rotate:random"));
+        Assert.assertEquals("!NS", ParsingUtil.parseToLegacyModifier("-flip:NS"));
+        Assert.assertEquals("!N", ParsingUtil.parseToLegacyModifier("-flip:N"));
+        Assert.assertEquals(":10", ParsingUtil.parseToLegacyModifier("-weight:10"));
+    }
+
+    @Test
+    public void legaceSchematicSetParserTest() {
+        Assert.assertEquals("&test@180:10", ParsingUtil.parseToLegacySyntax(new String[] {"\"p:test", "-rotate:180", "-weight:10\""})[0]);
+        Assert.assertEquals("&test@180!NE", ParsingUtil.parseToLegacySyntax(new String[] {"\"p:test", "-rotate:180","-flip:NE\""})[0]);
     }
 }
