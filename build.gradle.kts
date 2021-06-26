ada8d1cddf84daf5b13491b0cd1dd114a0135b26
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-26 16:52:46 +0200

    Introduce gradle. Bye maven o/

---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    java
+    `maven-publish`
+}
+
+repositories {
+    maven("https://eldonexus.de/repository/maven-public/")
+    maven("https://eldonexus.de/repository/maven-proxies/")
+}
+
+dependencies {
+    compileOnly("org.jetbrains","annotations","21.0.1")
+    implementation("de.eldoria", "eldo-util", "1.9.2-DEV")
+    compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.1.0")
+
+    testImplementation("org.jetbrains","annotations","21.0.1")
+    testImplementation("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.mockito:mockito-core:3.5.13")
+}
+
+group = "de.eldoria"
+version = "1.4.3"
+description = "SchematicBrushReborn"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+val shadebase = "de.eldoria.schematicbrush."
+val publishData = PublishData(project)
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifact(tasks["shadowJar"])
+        artifact(tasks["sourcesJar"])
+        artifact(tasks["javadocJar"])
+        groupId = project.group as String?
+        artifactId = project.name.toLowerCase()
+        version = publishData.getVersion()
+    }
+
+    repositories {
+        maven {
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
+                }
+            }
+
+            name = "EldoNexus"
+            url = uri(publishData.getRepository())
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileTestJava{
+        options.encoding = "UTF-8"
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+    shadowJar{
+        relocate("de.eldoutilities", shadebase + "eldoutilities")
+        mergeServiceFiles()
+    }
+    processResources {
+        from(sourceSets.main.get().resources.srcDirs) {
+            filesMatching("plugin.yml") {
+                expand(
+                    "version" to project.version
+                )
+            }
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+}
+
+class PublishData(private val project: Project) {
+    var type: Type = getReleaseType()
+    var hashLength: Int = 7
+
+    private fun getReleaseType(): Type {
+        val branch = getCheckedOutBranch()
+        return when {
+            branch.contentEquals("master") -> Type.RELEASE
+            branch.startsWith("dev") -> Type.DEV
+            else -> Type.SNAPSHOT
+        }
+    }
+
+    private fun getCheckedOutGitCommitHash(): String = System.getenv("GITHUB_SHA")?.substring(0, hashLength) ?: "local"
+
+    private fun getCheckedOutBranch(): String = System.getenv("GITHUB_REF")?.replace("refs/heads/", "") ?: "local"
+
+    fun getVersion(): String = getVersion(false)
+
+    fun getVersion(appendCommit: Boolean): String =
+        type.append(getVersionString(), appendCommit, getCheckedOutGitCommitHash())
+
+    private fun getVersionString(): String = (project.version as String).replace("-SNAPSHOT", "").replace("-DEV", "")
+
+    fun getRepository(): String = type.repo
+
+    enum class Type(private val append: String, val repo: String, private val addCommit: Boolean) {
+        RELEASE("", "https://eldonexus.de/repository/maven-releases/", false),
+        DEV("-DEV", "https://eldonexus.de/repository/maven-dev/", true),
+        SNAPSHOT("-SNAPSHOT", "https://eldonexus.de/repository/maven-snapshots/", true);
+
+        fun append(name: String, appendCommit: Boolean, commitHash: String): String =
+            name.plus(append).plus(if (appendCommit && addCommit) "-".plus(commitHash) else "")
+    }
+}
