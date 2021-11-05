5b1d6348c3ba8608ec46e21aad2aeb6b67ae1905
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-11-05 12:13:22 +0100

    Update dependencies

--- schematicbrushreborn-api/build.gradle.kts
+++ schematicbrushreborn-api/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("net.kyori", "adventure-api", "4.9.2")
+    api("de.eldoria", "eldo-util", "1.10.15-DEV")
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.0")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
--- schematicbrushreborn-core/build.gradle.kts
+++ schematicbrushreborn-core/build.gradle.kts
@@ -14,9 +14,10 @@ repositories {
 dependencies {
     implementation(project(":schematicbrushreborn-api"))
 
+    implementation("de.eldoria", "eldo-util", "1.10.15-DEV")
+    implementation("de.eldoria", "messageblocker", "1.0.3c-DEV")
     implementation("net.kyori", "adventure-platform-bukkit", "4.0.0")
     implementation("net.kyori", "adventure-text-minimessage", "4.2.0-SNAPSHOT")
-    implementation("de.eldoria", "messageblocker", "1.0.3c-DEV")
     testImplementation("org.jetbrains", "annotations", "21.0.1")
     testImplementation("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.1")
