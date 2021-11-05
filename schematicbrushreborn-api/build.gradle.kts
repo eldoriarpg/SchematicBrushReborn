b1c0953af0a02557252d987d7b24aa5aad95df85
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-11-05 15:31:11 +0100

    Adjust dependencies

--- schematicbrushreborn-api/build.gradle.kts
+++ schematicbrushreborn-api/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("net.kyori", "adventure-api", "4.9.2")
+    api("de.eldoria", "eldo-util", "1.10.15-DEV")
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.0")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
