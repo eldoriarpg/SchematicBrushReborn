fe8929e664498db312a01a557f5435bce82d4c8a
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-11-07 02:12:13 +0100

    Chores: Project Setup
    Fix typo in license header
    
    Add funding

--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/PasteMutationImpl.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/PasteMutationImpl.java
@@ -1,7 +1,7 @@
 /*
  *     SPDX-License-Identifier: AGPL-3.0-only
  *
- *     Copyright (C) 2021 EldoriaRPG Team und Contributor
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
  */
 
 package de.eldoria.schematicbrush.brush;
