a27b659952667ddd3068f16acf25d062ccc43802
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-12-24 00:05:46 +0100

    Fix build

--- buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains", "annotations", "20.1.0")
-    compileOnly("com.sk89q.worldedit", "worldedit-bukkit", "7.3.0-SNAPSHOT")
+    compileOnly("com.sk89q.worldedit", "worldedit-bukkit", "7.2.8")
 
     testImplementation(platform("org.junit:junit-bom:5.7.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
