30ec4bcdd8e2fc2a1186e23e964d23da48e647ce
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-04-28 17:50:47 +0200

    Add docs and license

--- schematicbrushreborn-api/src/main/java/de/eldoria/schematicbrush/brush/config/util/Copyable.java
+++ schematicbrushreborn-api/src/main/java/de/eldoria/schematicbrush/brush/config/util/Copyable.java
@@ -1,5 +1,18 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicbrush.brush.config.util;
 
 public interface Copyable {
+    /**
+     * Returns a copy of the implementing object.
+     *
+     * The copy should be by value and should not contain any mutable references to other objects.
+     *
+     * @return a copy of the object instance
+     */
     Object copy();
 }
