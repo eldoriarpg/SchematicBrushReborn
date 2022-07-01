ff260437945e9ec606e41a42063beae513b48a26
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-01 14:15:49 +0200

    Use bukkit-yml plugin

--- schematicbrushreborn-core/build.gradle.kts
+++ schematicbrushreborn-core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("de.eldoria.java-conventions")
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
 }
 
 val shadebase = "de.eldoria.schematicbrush.libs."
@@ -58,5 +59,84 @@ tasks {
 
     build {
         dependsOn(shadowJar)
+    }
+}
+
+bukkit {
+    name = "SchematicBrushReborn"
+    version = publishData.getVersion(true)
+    description = "Use your world edit schematics as a brush!"
+    apiVersion = "1.13"
+    main = "de.eldoria.schematicbrush.SchematicBrushRebornImpl"
+    authors = listOf("RainbowDashLabs", "SirYwell", "LuftigerLuca")
+    website = "https://www.spigotmc.org/resources/79441/"
+    depend = listOf("WorldEdit")
+    softDepend = listOf("ProtocolLib")
+
+    commands {
+        register("schematicbrushadmin") {
+            description = "Manage schematic brush"
+            aliases = listOf("sbra", "schbra")
+        }
+
+        register("schematicbrush") {
+            description = "Create and use schematic brushes"
+            aliases = listOf("sbr", "schbr", "schembrush")
+            permission = "schematicbrush.use"
+        }
+
+        register("schematicbrushsettings") {
+            description = "Manage the personal settings of schematic brush"
+            aliases = listOf("sbrs", "schbs")
+            permission = "schematicbrush.brush.use"
+        }
+
+        register("schematicbrushpreset") {
+            description = "List or edit schematic presets"
+            aliases = listOf("sbrp", "schbrp")
+            permission = "schematicbrush.preset.use"
+        }
+
+        register("schematicbrushbrushpreset") {
+            description = "List or edit brush presets"
+            aliases = listOf("sbrbp", "schbrbp")
+            permission = "schematicbrush.brushpreset.use"
+        }
+    }
+
+    permissions {
+        register("schematicbrush.*") {
+            description = "Allows full access to all schematic brush commands"
+        }
+        register("schematicbrush.brush.use") {
+            description = "Allows to use the schematic brush"
+        }
+        register("schematicbrush.brush.preview") {
+            description = "Allows to toggle and use preview"
+        }
+        register("schematicbrush.preset.use") {
+            description = "Access to read and use global brush presets and create and use private brush presets"
+        }
+        register("schematicbrush.preset.global") {
+            description = "Allows to create global presets"
+        }
+        register("schematicbrush.brushpreset.use") {
+            description = "Access to read and use global brush presets and create and use private brush presets"
+        }
+        register("schematicbrush.brushpreset.global") {
+            description = "Allows to create global brush presets"
+        }
+        register("schematicbrush.admin.reload") {
+            description = "Allows to reload the plugin. This might break add on integrations."
+        }
+        register("schematicbrush.admin.reloadcache") {
+            description = "Access to schematic cache reloading"
+        }
+        register("schematicbrush.admin.debug") {
+            description = "Access to debug command"
+        }
+        register("schematicbrush.admin.migrate") {
+            description = "Allows to migrate storage types"
+        }
     }
 }
