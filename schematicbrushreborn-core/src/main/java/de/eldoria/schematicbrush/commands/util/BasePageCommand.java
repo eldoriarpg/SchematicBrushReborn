316b4d538072868ec0962be6fd903a9551598c8b
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-01 13:54:30 +0200

    Fix license

--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/commands/util/BasePageCommand.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/commands/util/BasePageCommand.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicbrush.commands.util;
 
 import de.eldoria.eldoutilities.commands.command.AdvancedCommand;
