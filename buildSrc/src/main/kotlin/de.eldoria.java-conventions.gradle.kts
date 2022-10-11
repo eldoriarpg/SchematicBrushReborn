1f1451a8293c9e2ec006916fe76b52520a688654
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-10-11 12:11:57 +0200

    Cleanup buildfile at least a bit

--- buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.eldoria.java-conventions.gradle.kts
@@ -3,11 +3,6 @@ plugins {
 }
 
 repositories {
-    mavenCentral() {
-        content {
-            includeGroup("org.yaml")
-        }
-    }
     maven("https://eldonexus.de/repository/maven-public")
     maven("https://eldonexus.de/repository/maven-proxies")
 }
@@ -15,10 +10,11 @@ repositories {
 dependencies {
     compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains", "annotations", "23.0.0")
+    // Due to incompatibility by the yaml versions defined by world edit, fawe and bukkit we need to exclude it everywhere and add our own version...
+    compileOnly("org.yaml", "snakeyaml", "1.30")
     compileOnly("com.sk89q.worldedit", "worldedit-bukkit", "7.2.12") {
         exclude("org.yaml")
     }
-    compileOnly("org.yaml", "snakeyaml", "1.30")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.4.3") {
         exclude("com.intellectualsites.paster")
         exclude("org.yaml")
