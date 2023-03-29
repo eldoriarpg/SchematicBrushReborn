7b88da90114a69fc50e0b083db7527eace716f12
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-29 11:07:20 +0200

    Add license

--- schematicbrushreborn-api/src/main/java/de/eldoria/schematicbrush/config/sections/MessageChannel.java
+++ schematicbrushreborn-api/src/main/java/de/eldoria/schematicbrush/config/sections/MessageChannel.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicbrush.config.sections;
 
 public enum MessageChannel {
--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/serializer/FlipSerializer.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/serializer/FlipSerializer.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicbrush.config.serialization.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
