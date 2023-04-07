22471a03967e5e352b81d370a090f57ba81bfcf3
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-04-07 17:12:15 +0200

    Apply license

--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/deserilizer/SchematicSetBuilderDeserializer.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/deserilizer/SchematicSetBuilderDeserializer.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicbrush.config.serialization.deserilizer;
 
 import com.fasterxml.jackson.core.JacksonException;
