2326c2e9df59fbc5b76068e2f65348d942026223
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-04-07 16:02:06 +0200

    Add serializer for set and brush container

---
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/deserilizer/BrushBuilderSnapshotDeserializer.java
@@ -0,0 +1,17 @@
+package de.eldoria.schematicbrush.config.serialization.deserilizer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import de.eldoria.schematicbrush.brush.config.builder.BrushBuilderSnapshot;
+import de.eldoria.schematicbrush.brush.config.builder.BrushBuilderSnapshotImpl;
+
+import java.io.IOException;
+
+public class BrushBuilderSnapshotDeserializer extends JsonDeserializer<BrushBuilderSnapshot> {
+    @Override
+    public BrushBuilderSnapshot deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        return ctxt.readValue(p, BrushBuilderSnapshotImpl.class);
+    }
+}
---
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/config/serialization/deserilizer/SchematicSetBuilderDeserializer.java
@@ -0,0 +1,17 @@
+package de.eldoria.schematicbrush.config.serialization.deserilizer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import de.eldoria.schematicbrush.brush.config.builder.SchematicSetBuilder;
+import de.eldoria.schematicbrush.brush.config.builder.SchematicSetBuilderImpl;
+
+import java.io.IOException;
+
+public class SchematicSetBuilderDeserializer extends JsonDeserializer<SchematicSetBuilder> {
+    @Override
+    public SchematicSetBuilder deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        return ctxt.readValue(p, SchematicSetBuilderImpl.class);
+    }
+}
