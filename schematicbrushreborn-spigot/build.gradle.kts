5ec1b5a9fa2c5ce3158594844387beb9a0e421b1
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-06-16 12:45:15 +0200

    Migrate plugins

--- schematicbrushreborn-spigot/build.gradle.kts
+++ schematicbrushreborn-spigot/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    alias(libs.plugins.pluginyml.bukkit)
+    alias(libs.plugins.shadow)
 }
 
 val shadebase = "de.eldoria.schematicbrush.libs."
