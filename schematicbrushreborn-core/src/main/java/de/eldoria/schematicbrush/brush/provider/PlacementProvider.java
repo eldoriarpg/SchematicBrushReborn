f42b1e981c2c9d84d95effbe15385c81e198d7c9
Zoriot <zoriot@proton.me>
2026-05-15 00:56:15 +0200

    ♻️ Avoid unnecessary allocation

--- schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/provider/PlacementProvider.java
+++ schematicbrushreborn-core/src/main/java/de/eldoria/schematicbrush/brush/provider/PlacementProvider.java
@@ -56,7 +56,7 @@ public abstract class PlacementProvider extends ModifierProvider {
 
     @Override
     public Mutator<?> defaultSetting() {
-        return new Drop();
+        return drop;
     }
 
     @Override
